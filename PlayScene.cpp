1c8870f91b7ad12feed0c70bf34a9e839bd43a7b
八基 後藤 <220385@jc-21.jp>
2024-02-05 09:16:42 +0900

    水がなかった

--- PlayScene.cpp
+++ PlayScene.cpp
@@ -5,6 +5,7 @@
 #include "Controller.h"
 #include "Torus.h"
 #include "Engine/Input.h"
+#include "Water.h"
 
 namespace {
 	const XMFLOAT4 DEF_LIGHT_POSITION{ 1, 2, 1, 0 };
@@ -41,7 +42,7 @@ void PlayScene::Initialize()
 {
 	pCtl = (Controller*)Instantiate<Controller>(this);
 	//Instantiate<Torus>(this);
-	Instantiate<Ball>(this);
+	Instantiate<Water>(this);
 	//pArrow = (Arrow*)Instantiate<Arrow>(this);
 	//XMFLOAT3 BallPos = pCtl->GetPosition();
 	//XMFLOAT3 BallPosNormal;
